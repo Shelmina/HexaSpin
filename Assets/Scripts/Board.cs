64883cb8aa01644672fdddd6fd2994f597720d79
meryemkardelen <meryemkardelenn@gmail.com>
2020-09-16 21:14:08 +0300

    needs debug
    coloring failure

--- Assets/Scripts/Board.cs
+++ Assets/Scripts/Board.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 
 public class Board : MonoBehaviour
 {
+    public int difficulty = 3;
     public Color [] colors;
     public GameObject thisBoard;
     public GameObject hexagon;
@@ -12,12 +13,15 @@ public class Board : MonoBehaviour
     public int quantity;
     public float xPosition;
     public float yPosition;
+    int column = 0;
+    int row = 0;
+    int rand;
   
     // Start is called before the first frame update
     void Start()
     {
+        tiles = new GameObject[quantity / 4, 8];
         CreateHexTiles(xPosition, yPosition);
-        ColorHexTiles();
     }
 
     // Update is called once per frame
@@ -27,36 +31,72 @@ public class Board : MonoBehaviour
     }
     private void CreateHexTiles(float xCoord, float yCoord)
         {
-        int column = 0;
-        int row = 0;
-        for (int i = 0; i < quantity; i++)
+        for (int i = 0; i < quantity*2; i++)
             {
-            //bir tile olusturup rotasyonunu degistiriyorum
-            
-            tiles[row, column] = (GameObject)Instantiate(hexagon, new Vector2(xCoord, yCoord), Quaternion.identity) as GameObject;
-            tiles[row, column].transform.rotation = Quaternion.Euler(0f, 0f, 90f);
-            tiles[row, column].transform.parent = thisBoard.transform;
-            tiles[row, column].name = "( " + row + ", " + column + ")";
-            column++;
-            //ayni dongu icerisinde capraz sekilde digerlerini de olusturuyorum
-            tiles[row, column] = (GameObject)Instantiate(hexagon, new Vector2(xCoord, yCoord), Quaternion.identity) as GameObject;
-            tiles[row, column].transform.rotation = Quaternion.Euler(0f, 0f, 90f);
-            tiles[row, column].transform.parent = thisBoard.transform;
-            tiles[row, column].name = "( " + row + ", " + column + ")";
-            column++;
-            xCoord += 1.1f; //ayni siradaki blokların arasındaki x mesafesi
+            CreateTile(xCoord, yCoord);
+            ColorHexTiles(row, column);
+            column += 2;
+            xCoord += 1.1f;
             if (column == 8)
             {
+                column = 1;
+                xCoord = xPosition + 0.55f;
+                yCoord += 0.31f;
+            }
+            if (column == 9)
+            {
                 row++;
-                column = 0;
                 xCoord = xPosition;
-                yCoord += 0.62f; //ayni sutundaki blokların arasındaki y mesafesi
+                yCoord += 0.31f;
+                column = 0;
             }
         }
  
         }
-    private void ColorHexTiles()
+    private void CreateTile(float xPos, float yPos) {
+        tiles[row, column] = (GameObject)Instantiate(hexagon, new Vector2(xPos, yPos), Quaternion.identity) as GameObject;
+        tiles[row, column].transform.rotation = Quaternion.Euler(0f, 0f, 90f);
+        tiles[row, column].transform.parent = thisBoard.transform;
+        tiles[row, column].name = "( " + row + ", " + column + ")";
+    }
+    private void ColorHexTiles(int x, int y)
     {
-
+        rand = Random.Range(0, difficulty);
+        if ((x == 0)||(y == 0))
+        {
+            tiles[x, y].GetComponent<SpriteRenderer>().color = colors[rand];
+            return;
+        }
+        
+        else
+        {
+        if (y == 7)
+        {
+            while (tiles[x, y - 1].GetComponent<SpriteRenderer>().color == tiles[x - 1, y].GetComponent<SpriteRenderer>().color)
+            {
+                rand = Random.Range(0, difficulty);
+            }
+            tiles[x, y].GetComponent<SpriteRenderer>().color = colors[rand];
+            return;
+        }
+        if (y % 2 == 1)
+        {
+            while ((tiles[x, y - 1].GetComponent<SpriteRenderer>().color == tiles[x - 1, y].GetComponent<SpriteRenderer>().color)
+            || (tiles[x - 1, y].GetComponent<SpriteRenderer>().color == tiles[x, y + 1].GetComponent<SpriteRenderer>().color))
+            {
+                rand = Random.Range(0, difficulty);
+            }
+        }
+        else
+        {
+            while ((tiles[x - 1, y - 1].GetComponent<SpriteRenderer>().color == tiles[x - 1, y].GetComponent<SpriteRenderer>().color)
+            || (tiles[x - 1, y].GetComponent<SpriteRenderer>().color == tiles[x - 1, y + 1].GetComponent<SpriteRenderer>().color))
+            {
+                rand = Random.Range(0, difficulty);
+            }
+        }
+        tiles[x, y].GetComponent<SpriteRenderer>().color = colors[rand];
+        return;
+        }
     }
 }
