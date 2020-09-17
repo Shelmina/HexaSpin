e952df7f5e8c6e2ea0510a780c10b8bfad903333
meryemkardelen <meryemkardelenn@gmail.com>
2020-09-17 13:53:28 +0300

    Grid base constructed
    Coloring bugs fixed. Algorithm approved.

--- Assets/Scripts/Board.cs
+++ Assets/Scripts/Board.cs
@@ -6,7 +6,7 @@ using UnityEngine;
 public class Board : MonoBehaviour
 {
     public int difficulty = 3;
-    public Color [] colors;
+    public Color[] colors;
     public GameObject thisBoard;
     public GameObject hexagon;
     GameObject[,] tiles;
@@ -16,7 +16,7 @@ public class Board : MonoBehaviour
     int column = 0;
     int row = 0;
     int rand;
-  
+
     // Start is called before the first frame update
     void Start()
     {
@@ -27,12 +27,12 @@ public class Board : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+
     }
     private void CreateHexTiles(float xCoord, float yCoord)
+    {
+        for (int i = 0; i < quantity * 2; i++)
         {
-        for (int i = 0; i < quantity*2; i++)
-            {
             CreateTile(xCoord, yCoord);
             ColorHexTiles(row, column);
             column += 2;
@@ -51,9 +51,10 @@ public class Board : MonoBehaviour
                 column = 0;
             }
         }
- 
-        }
-    private void CreateTile(float xPos, float yPos) {
+
+    }
+    private void CreateTile(float xPos, float yPos)
+    {
         tiles[row, column] = (GameObject)Instantiate(hexagon, new Vector2(xPos, yPos), Quaternion.identity) as GameObject;
         tiles[row, column].transform.rotation = Quaternion.Euler(0f, 0f, 90f);
         tiles[row, column].transform.parent = thisBoard.transform;
@@ -61,42 +62,28 @@ public class Board : MonoBehaviour
     }
     private void ColorHexTiles(int x, int y)
     {
-        rand = Random.Range(0, difficulty);
-        if ((x == 0)||(y == 0))
+        int counter;
+        do
         {
-            tiles[x, y].GetComponent<SpriteRenderer>().color = colors[rand];
-            return;
-        }
-        
-        else
-        {
-        if (y == 7)
-        {
-            while (tiles[x, y - 1].GetComponent<SpriteRenderer>().color == tiles[x - 1, y].GetComponent<SpriteRenderer>().color)
-            {
-                rand = Random.Range(0, difficulty);
-            }
-            tiles[x, y].GetComponent<SpriteRenderer>().color = colors[rand];
-            return;
-        }
-        if (y % 2 == 1)
-        {
-            while ((tiles[x, y - 1].GetComponent<SpriteRenderer>().color == tiles[x - 1, y].GetComponent<SpriteRenderer>().color)
-            || (tiles[x - 1, y].GetComponent<SpriteRenderer>().color == tiles[x, y + 1].GetComponent<SpriteRenderer>().color))
-            {
-                rand = Random.Range(0, difficulty);
-            }
-        }
-        else
-        {
-            while ((tiles[x - 1, y - 1].GetComponent<SpriteRenderer>().color == tiles[x - 1, y].GetComponent<SpriteRenderer>().color)
-            || (tiles[x - 1, y].GetComponent<SpriteRenderer>().color == tiles[x - 1, y + 1].GetComponent<SpriteRenderer>().color))
-            {
-                rand = Random.Range(0, difficulty);
-            }
-        }
+            rand = Random.Range(0, difficulty);
+            counter = 0;
+            //sol alt tek
+            if (y > 0 && (y % 2) == 1 && (colors[rand] == tiles[x, y - 1].GetComponent<SpriteRenderer>().color))
+                counter++;
+            //sol alt cift
+            else if (y > 0 && (y % 2) == 0 && x > 0 && (colors[rand] == tiles[x - 1, y - 1].GetComponent<SpriteRenderer>().color))
+                counter++;
+            //alt
+            if (x > 0 && (colors[rand] == tiles[x - 1, y].GetComponent<SpriteRenderer>().color))
+                counter++;
+            //sag alt tek
+            if (y < 7 && x > 0 && (y % 2 == 1) && (colors[rand] == tiles[x, y + 1].GetComponent<SpriteRenderer>().color))
+                counter++;
+            else if (y < 7 && x > 0 && (y % 2 == 0) && (colors[rand] == tiles[x - 1, y + 1].GetComponent<SpriteRenderer>().color))
+                counter++;
+        } while (counter >= 2);
         tiles[x, y].GetComponent<SpriteRenderer>().color = colors[rand];
-        return;
-        }
     }
 }
+
+
