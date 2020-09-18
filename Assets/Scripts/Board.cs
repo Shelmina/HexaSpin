db71405955f78d002e0b95c4ce56f485481f3779
meryemkardelen <meryemkardelenn@gmail.com>
2020-09-18 19:51:25 +0300

    Weekly commit
    Point reading added

--- Assets/Scripts/Board.cs
+++ Assets/Scripts/Board.cs
@@ -1,7 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
-using UnityEditor.Tilemaps;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Board : MonoBehaviour
 {
@@ -13,10 +13,10 @@ public class Board : MonoBehaviour
     public int quantity;
     public float xPosition;
     public float yPosition;
+    public Text testing;
     int column = 0;
     int row = 0;
     int rand;
-
     // Start is called before the first frame update
     void Start()
     {
@@ -27,13 +27,53 @@ public class Board : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        /*if (Input.touchCount > 0)
+        {
+            Vector3 wp = Camera.main.ScreenToWorldPoint(Input.GetTouch(0).position);
+            Vector2 touchPos = new Vector2(wp.x, wp.y);
+            //print(Input.touchCount);
+            testing.text = touchPos.ToString();
+        }*/
+        if((Input.touchCount > 0) && Input.touches[0].phase == TouchPhase.Began)
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.touches[0].position);
+            RaycastHit hit;
+            if(Physics.Raycast(ray, out hit))
+            {
+                if(hit.collider != null)
+                {
+                    Color newColor = new Color(Random.Range(0.0f, 1.0f), Random.Range(0.0f, 1.0f), Random.Range(0.0f, 1.0f));
+                    hit.collider.GetComponent<MeshRenderer>().material.color = newColor;
+                    hit.transform.position = new Vector2(0f, 0f);
+                    testing.text = Input.touchCount.ToString();
+                }
+            }
+        }
+#if UNITY_EDITOR
+        if (Input.GetMouseButtonDown(0))
+        {
+            // Ray2D ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            /*RaycastHit2D hit;
 
+            if (hit = Physics2D.Raycast(Camera.main.transform.position, Input.mousePosition))
+
+                    //Raycast(ray, Vector2.zero, out hit))
+            {*/
+            RaycastHit2D hit = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(Input.mousePosition), Vector2.zero);
+                if (hit.collider != null)
+                {
+                    testing.text = hit.transform.name;
+                }
+            
+        }
+#endif
     }
     private void CreateHexTiles(float xCoord, float yCoord)
     {
         for (int i = 0; i < quantity * 2; i++)
         {
             CreateTile(xCoord, yCoord);
+            tiles[row, column].name = "( " + row + ", " + column + ")";
             ColorHexTiles(row, column);
             column += 2;
             xCoord += 1.1f;
@@ -56,9 +96,8 @@ public class Board : MonoBehaviour
     private void CreateTile(float xPos, float yPos)
     {
         tiles[row, column] = (GameObject)Instantiate(hexagon, new Vector2(xPos, yPos), Quaternion.identity) as GameObject;
-        tiles[row, column].transform.rotation = Quaternion.Euler(0f, 0f, 90f);
+        //tiles[row, column].transform.rotation = Quaternion.Euler(0f, 0f, 90f);
         tiles[row, column].transform.parent = thisBoard.transform;
-        tiles[row, column].name = "( " + row + ", " + column + ")";
     }
     private void ColorHexTiles(int x, int y)
     {
