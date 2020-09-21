321b7489054a1552fdca017ec571db31171b021c
meryemkardelen <meryemkardelenn@gmail.com>
2020-09-21 17:43:52 +0300

    bug fix

--- Assets/Scripts/Board.cs
+++ Assets/Scripts/Board.cs
@@ -1,53 +1,18 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
 
 public class Board : MonoBehaviour
 {
-    private HexTile hexTile;
-    public Text testing;
+    private HexagonManager hexagonManager;
     void Start()
     {
-        hexTile = GetComponent<HexTile>();
-        hexTile.ConstructBoardTiles(); 
+        hexagonManager = GetComponent<HexagonManager>();
+        hexagonManager.ConstructBoardTiles(); 
     }
     void Update()
     {
-        /*if (Input.touchCount > 0)
-        {
-            Vector3 wp = Camera.main.ScreenToWorldPoint(Input.GetTouch(0).position);
-            Vector2 touchPos = new Vector2(wp.x, wp.y);
-            //print(Input.touchCount);
-            testing.text = touchPos.ToString();
-        }*/
-        if((Input.touchCount > 0) && Input.touches[0].phase == TouchPhase.Began)
-        {
-            RaycastHit2D hit = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(Input.touches[0].position), Vector2.zero);
-            if (hit.collider != null)
-            {
-                testing.text = hit.transform.name;
-            }
-        }
-#if UNITY_EDITOR
-        if (Input.GetMouseButtonDown(0))
-        {
-            // Ray2D ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-            /*RaycastHit2D hit;
-
-            if (hit = Physics2D.Raycast(Camera.main.transform.position, Input.mousePosition))
-
-                    //Raycast(ray, Vector2.zero, out hit))
-            {*/
-            RaycastHit2D hit = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(Input.mousePosition), Vector2.zero);
-                if (hit.collider != null)
-                {
-                    testing.text = hit.transform.name;
-                }
-            
-        }
-#endif
+       
     }
 }
-
-
